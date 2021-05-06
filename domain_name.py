0e9b3c4da71d1568010aa00356c91b0c36e11df8
Akshay2210 <62205256+Akshay2210@users.noreply.github.com>
2021-05-06 03:36:04 +0000

    Delete domain_name.py

--- domain_name.py
+++
@@ -1,8 +0,0 @@
-from tld import get_fld
-
-def get_domain_name(url):
-    domain_name = get_fld(url)
-    print('-------------------------------------')
-    print("Domain Name is: "+domain_name)
-    return domain_name
-
