50108824513717c458c9c88b2794b3e3bae0a6e4
Akshay2210 <62205256+Akshay2210@users.noreply.github.com>
2021-06-12 20:37:01 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 echo 'Installing Web Scanner....'
-sudo apt-get update
-sudo apt-get install python3-pip (Python3)
+sudo apt-get update -y
+sudo apt-get install python3-pip -y
 python3 -m pip install -r requirements.txt
-sudo apt-get install nikto
-sudo apt-get install nmap
-sudo apt-get install whois
-sudo apt-get install traceroute
+sudo apt-get install nikto -y
+sudo apt-get install nmap -y
+sudo apt-get install whois -y
+sudo apt-get install traceroute -y
