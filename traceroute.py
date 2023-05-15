8b9da500c1885563545aaa1c2e7f6588e1453bd8
Akshay7591 <62205256+Akshay7591@users.noreply.github.com>
2023-05-15 11:46:25 +0530

    Update traceroute.py

--- traceroute.py
+++ traceroute.py
@@ -6,13 +6,3 @@ def tracert(x):
     print('-------------------')
     print('Traceroute-')
     os.system('traceroute '+x)
-    '''
-    command='traceroute '+x
-    process=os.popen(command)
-    results=str(process.read())
-    print(results)
-    return results
-    '''
-
-#x=str(input("Enter website:"))
-#tracert(x)
